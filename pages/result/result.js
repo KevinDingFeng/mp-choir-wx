5f6a0bf36be7dfa90ba5ebbafa8c1bfa0ae4ce2a
EDZ <hex@shenghesun.com>
2018-08-28 16:02:16 +0800

    update

--- pages/result/result.js
+++ pages/result/result.js
@@ -8,13 +8,16 @@ Page({
   data: {
       pageStyle: `width:${app.globalData.width};height:${app.globalData.height}`,
       scale: app.globalData.windowWidth / app.globalData.windowHeight,
+      hbImagePath:null,
+      canvasHidden:true,
+      picPath:'../../images/picker.png'
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    
   },
 
   /**
@@ -64,5 +67,69 @@ Page({
    */
   onShareAppMessage: function () {
   
+  },
+
+  getHb:function(){
+    this.setData({
+      canvasHidden:false
+    });
+    const ctx = wx.createCanvasContext('resultCanvas');
+    var s = this;
+    var id = 1;
+    wx.request({
+      url: 'http://192.168.3.37:9090/syn_songs/' + id + '/wxacode',
+      success: function (res) {
+        wx.getImageInfo({
+          src: res.data.data,
+          success:function(res){
+            console.log("开始导出图片");
+            ctx.drawImage(res.path, 0, 0, 150, 100);
+            console.log("绘制完成");
+            ctx.draw(true, setTimeout(function () {
+              console.log("开始导出");
+              wx.canvasToTempFilePath({
+                x: 0,
+                y: 0,
+                width: 150,
+                height: 100,
+                destWidth: 100,
+                destHeight: 100,
+                canvasId: 'resultCanvas',
+                success: function (res) {
+                  console.log(res.tempFilePath);
+                  wx.saveImageToPhotosAlbum({
+                    filePath: res.tempFilePath,
+                    success:function(res){
+                      console.log("保存到相册成功");
+                      s.setData({
+                        canvasHidden: true
+                      });
+                    }
+                  })
+                },
+                fail(res) {
+                  console.log("fail");
+                }
+              });
+            }, 100));
+          }
+        });
+        
+      }
+    });
+    
+  },
+
+  alterPic:function(){
+    var s = this;
+    wx.chooseImage({
+      count:1,
+      success: function(res) {
+        console.log(res);
+        s.setData({
+          picPath: res.tempFilePaths[0]
+        });
+      },
+    })
   }
 })
