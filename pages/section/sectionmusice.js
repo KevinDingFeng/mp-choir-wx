5e5d24f428803b4827cac6e5e8f726fd9d8c23cb
wht <wanght@shenghesun.com>
2018-09-11 12:14:16 +0800

    update

--- pages/section/sectionmusice.js
+++ pages/section/sectionmusice.js
@@ -1,7 +1,7 @@
 // pages/section/sectionmusice.js
 var config = require('../../utils/config.js');
 const app = getApp()
-let couPeo = false; 
+let couPeo = false;
 Page({
 
     /**
@@ -24,13 +24,13 @@ Page({
         compound: false, //点击合成后弹出的窗口
         loginUserId: wx.getStorageSync('userId'),
         bj_img: config.bg_img + "/04bg.png",
-      cou_peo: true  //人数不够点我来凑
+        cou_peo: true  //人数不够点我来凑
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
-    onLoad: function(options) {
+    onLoad: function (options) {
         let that = this;
         if (options.choirId) {
             that.setData({
@@ -42,11 +42,11 @@ Page({
                 data: {
                     choirId: options.choirId
                 },
-                success: function(res) {
+                success: function (res) {
                     console.log(res.data)
                     let resData = res.data;
                     if (resData && resData.success) {
-                      if (resData.data.users[0].id == that.data.loginUserId) {
+                        if (resData.data.users[0].id == that.data.loginUserId) {
                             that.setData({
                                 sponsor: true
                             })
@@ -55,25 +55,25 @@ Page({
                         for (var i = 0; i < data.songSection.length; i++) {
                             data.songSection[i].bf_img = "muscie_f.png";
                             data.songSection[i].bf_type = "1";
-                          if (data.songSection[i].status == "NO_CLAIM") {
-                            couPeo = true;
-                          }
+                            if (data.songSection[i].status == "NO_CLAIM") {
+                                couPeo = true;
+                            }
                         }
-                      if (couPeo) {
-                        that.setData({
-                          cou_peo: true
-                        })
-                      } else {
-                        that.setData({
-                          cou_peo: false
-                        })
-                      }
+                        if (couPeo) {
+                            that.setData({
+                                cou_peo: true
+                            })
+                        } else {
+                            that.setData({
+                                cou_peo: false
+                            })
+                        }
                         that.setData({
                             result: data
                         })
                     }
                 },
-                fail: function(e) {
+                fail: function (e) {
                     console.log(e);
                 }
             })
@@ -82,54 +82,54 @@ Page({
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
-    onReady: function() {
+    onReady: function () {
 
     },
     /**
      * 生命周期函数--监听页面显示
      */
-    onShow: function() {
+    onShow: function () {
         let that = this;
         let array = that.data.result;
-        if(array.length){
-          for (var i = 0; i < array.songSection.length; i++) {
-            array.songSection[i].bf_img = "muscie_f.png";
-            array.songSection[i].bf_type = "1";
-          }
-          that.setData({
-            result: array
-          })
+        if (array.length) {
+            for (var i = 0; i < array.songSection.length; i++) {
+                array.songSection[i].bf_img = "muscie_f.png";
+                array.songSection[i].bf_type = "1";
+            }
+            that.setData({
+                result: array
+            })
         }
-        
+
     },
-    goback: function() {
+    goback: function () {
         let that = this;
-      var scene = wx.getStorageSync('scene');
-      if (scene && (scene != 1001 )) {
-        wx.removeStorageSync("scene");
-        wx.redirectTo({
-          url: '/pages/index/index',
-        })
-        return
-      }
-      if (wx.getStorageSync('myWriting')){
-        wx.removeStorageSync("myWriting");
-        wx.redirectTo({
-          url: '/pages/myWriting/myWriting',
-        })
-        return
-      }
+        var scene = wx.getStorageSync('scene');
+        if (scene && (scene != 1001)) {
+            wx.removeStorageSync("scene");
+            wx.redirectTo({
+                url: '/pages/index/index',
+            })
+            return
+        }
+        if (wx.getStorageSync('myWriting')) {
+            wx.removeStorageSync("myWriting");
+            wx.redirectTo({
+                url: '/pages/myWriting/myWriting',
+            })
+            return
+        }
         wx.navigateTo({
             url: '/pages/choose/choosemusice?choirId=' + that.data.choirId + '&section=1',
         })
     },
-  gohome: function () {
-    wx.removeStorageSync("myWriting");
-    wx.navigateTo({
-      url: '/pages/index/index',
-    })
-  },
-    createAudio: function(event) {
+    gohome: function () {
+        wx.removeStorageSync("myWriting");
+        wx.navigateTo({
+            url: '/pages/index/index',
+        })
+    },
+    createAudio: function (event) {
         let that = this;
         let currId = event.currentTarget.dataset.id;
         let _type = event.currentTarget.dataset.type;
@@ -144,7 +144,7 @@ Page({
                 if (array.songSection[i].id == currId) {
                     array.songSection[i].bf_img = "muscie_t.png";
                     array.songSection[i].bf_type = "2";
-                }else{
+                } else {
                     array.songSection[i].bf_img = "muscie_f.png";
                     array.songSection[i].bf_type = "1";
                 }
@@ -167,7 +167,7 @@ Page({
         }
     },
     //认领歌曲
-    claim: function(event) {
+    claim: function (event) {
         //console.log(event)
         let that = this;
         const id = event.target.dataset.id;
@@ -175,50 +175,50 @@ Page({
         wx.request({
             url: config.baseUrl + '/song_section/claim', //
             data: {
-              id: id,
-              userId: that.data.loginUserId,
-              avatarUrl: app.globalData.userInfo.avatarUrl
+                id: id,
+                userId: that.data.loginUserId,
+                avatarUrl: app.globalData.userInfo.avatarUrl
             },
-            success: function(res) {
+            success: function (res) {
                 //console.log(res.data)
                 let resData = res.data;
                 if (resData && resData.errorCode == 0) {
                     var result = that.data.result;
                     var songSections = result.songSection;
-                    
+
                     for (var i = 0; i < songSections.length; i++) {
                         if (songSections[i].id == id) {
-                          songSections[i].userId = that.data.loginUserId;
-                          songSections[i].status = "NO_RECORDING";
-                          songSections[i].avatarUrl = app.globalData.userInfo.avatarUrl;
+                            songSections[i].userId = that.data.loginUserId;
+                            songSections[i].status = "NO_RECORDING";
+                            songSections[i].avatarUrl = app.globalData.userInfo.avatarUrl;
                         }
-                      
-                      if (songSections[i].status =="NO_CLAIM"){
-                        couPeo = true;
-                      }
+
+                        if (songSections[i].status == "NO_CLAIM") {
+                            couPeo = true;
+                        }
                     }
-                  if (couPeo){
-                    that.setData({
-                      cou_peo: true
-                    })
-                  }else{
-                    that.setData({
-                      cou_peo: false
-                    })
-                  }
+                    if (couPeo) {
+                        that.setData({
+                            cou_peo: true
+                        })
+                    } else {
+                        that.setData({
+                            cou_peo: false
+                        })
+                    }
                     that.setData({
                         result: result
                     })
                     console.log(that.data.result)
                 }
             },
-            fail: function(e) {
+            fail: function (e) {
                 console.log(e);
             }
         })
     },
     //演唱
-    sing: function(event) {
+    sing: function (event) {
         let that = this;
         const id = event.target.dataset.id;
         const name = event.target.dataset.name;
@@ -226,83 +226,105 @@ Page({
         const sort = event.target.dataset.sort;
         const choirid = event.target.dataset.choirid;
         wx.navigateTo({
-          url: '/pages/c_musice/c_musice?name=' + name + "&population=" + population + "&sort=" + sort + "&id=" + id + "&choirid=" + choirid,
+            url: '/pages/c_musice/c_musice?name=' + name + "&population=" + population + "&sort=" + sort + "&id=" + id + "&choirid=" + choirid,
         })
     },
     //合成
-    compound: function() {
+    compound: function () {
         let that = this;
         that.setData({
             compound: true
         })
     },
-    zt_musice: function() {
+    zt_musice: function () {
         wx.pauseBackgroundAudio()
     },
     /**
      * 生命周期函数--监听页面隐藏
      */
-    onHide: function() {
+    onHide: function () {
         wx.stopBackgroundAudio();//停止播放
     },
 
     /**
      * 生命周期函数--监听页面卸载
      */
-    onUnload: function() {
+    onUnload: function () {
         wx.stopBackgroundAudio();//停止播放
     },
 
     /**
      * 页面相关事件处理函数--监听用户下拉动作
      */
-    onPullDownRefresh: function() {
+    onPullDownRefresh: function () {
 
     },
 
     /**
      * 页面上拉触底事件的处理函数
      */
-    onReachBottom: function() {
+    onReachBottom: function () {
 
     },
 
     /**
      * 用户点击右上角分享
      */
-    onShareAppMessage: function(res) {
-      let that = this;
+    onShareAppMessage: function (res) {
+        let that = this;
         if (res.from === 'button') {
-          // 来自页面内转发按钮
-          //console.log(res.target)
-          if (res.target.dataset.sharevalue) {
-            wx.request({
-              url: config.baseUrl + '/syn_songs/compound', //
-              data: {
-                choirId: that.data.choirId
-              },
-              success: function (res) {
-                console.log(res.data)
-                let resData = res.data;
-                if (resData && resData.success) {
-                  wx.navigateTo({
-                    url: '/pages/result/result?choirId=' + that.data.choirId,
-                  })
-                }
-              },
-              fail: function (e) {
-                console.log(e);
-              }
+            let array = that.data.result;
+            for (var i = 0; i < array.songSection.length; i++) {
+                // if (array.songSection[i].id == currId) {
+                array.songSection[i].bf_img = "muscie_f.png";
+                array.songSection[i].bf_type = "1";
+                // }
+            }
+            that.setData({
+                result: array
             })
-          }
-          that.setData({
-            sponsor: false
-          })
+            wx.stopBackgroundAudio();//停止播放
+            // 来自页面内转发按钮
+            //console.log(res.target)
+            if (res.target.dataset.sharevalue) {
+                let array = that.data.result;
+                for (var i = 0; i < array.songSection.length; i++) {
+                    // if (array.songSection[i].id == currId) {
+                    array.songSection[i].bf_img = "muscie_f.png";
+                    array.songSection[i].bf_type = "1";
+                    // }
+                }
+                that.setData({
+                    result: array
+                })
+                wx.stopBackgroundAudio();//停止播放
+                wx.request({
+                    url: config.baseUrl + '/syn_songs/compound', //
+                    data: {
+                        choirId: that.data.choirId
+                    },
+                    success: function (res) {
+                        console.log(res.data)
+                        let resData = res.data;
+                        if (resData && resData.success) {
+                            wx.navigateTo({
+                                url: '/pages/result/result?choirId=' + that.data.choirId,
+                            })
+                        }
+                    },
+                    fail: function (e) {
+                        console.log(e);
+                    }
+                })
+            }
+            that.setData({
+                sponsor: false
+            })
         } else {
-          return {
-            title: "",
-            path: '/pages/index/index'
-          }
-        } 
+            return {
+                title: "",
+                path: '/pages/index/index'
+            }
+        }
     }
 })
