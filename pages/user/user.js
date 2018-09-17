562fc76e2bdaebf1963ec00bf5ab1ce814194bfa
yangzp <yangzp@shenghesun.com>
2018-09-17 16:36:15 +0800

    update

--- pages/user/user.js
+++ pages/user/user.js
@@ -28,10 +28,14 @@ Page({
   // 输入团名
   bindChoirName: function (e) {
     var this_ = this;
-    var regStr = /[\uD83C|\uD83D|\uD83E][\uDC00-\uDFFF][\u200D|\uFE0F]|[\uD83C|\uD83D|\uD83E][\uDC00-\uDFFF]|[0-9|*|#]\uFE0F\u20E3|[0-9|#]\u20E3|[\u203C-\u3299]\uFE0F\u200D|[\u203C-\u3299]\uFE0F|[\u2122-\u2B55]|\u303D|[\A9|\AE]\u3030|\uA9|\uAE|\u3030/ig;
+    var regStr = /[\uD83C|\uD83D|\uD83E][\uDC00-\uDFFF][\u200D|\uFE0F]|[\uD83C|\uD83D|\uD83E][\uDC00-\uDFFF]|[0-9|*|#]\uFE0F\u20E3|[0-9|#]\u20E3|[\u203C-\u3299]\uFE0F\u200D|[\u203C-\u3299]\uFE0F|[\u2122-\u2B55]|\u303D|[\A9|\AE]\u3030|\uA9|\uAE|\u3030/ig;;
     if (regStr.test(e.detail.value)) {
       this_.setData({
         'choir.choirName': e.detail.value.replace(regStr, "")
+      });
+    }else{
+      this_.setData({
+        'choir.choirName': e.detail.value
       });
     }
     
@@ -116,7 +120,7 @@ Page({
     if (!choirName) {
       _this.setData({
         'choir.choirName': ''
-      })
+      });
       warn = "请输入你们的团名~";
     } else if (!population) {
       warn = "请选择你们的团人数~";
